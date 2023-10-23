9fa46d25eaf0538d5419a73fed2ba9ad1a0e8de4
Pranav Prasanna <77316684+pranavp08@users.noreply.github.com>
2023-10-23 22:05:11 +0530

    Included username in user model

--- quillpost/auth/users/models.py
+++ quillpost/auth/users/models.py
@@ -3,10 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
-    username = None
-
-    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS=[]
--- quillpost/auth/users/serializers.py
+++ quillpost/auth/users/serializers.py
@@ -4,7 +4,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'name', 'email', 'password']
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password']
         extra_kwargs = {
             'password' : {'write_only' : True}
         }
