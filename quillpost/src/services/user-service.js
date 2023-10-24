f8973dfa7d7660d0f25223c80de76ea5b62d23b6
KUNAL <kunalsingh2703@gmail.com>
2023-10-24 19:11:50 +0530

    Login page connect with api

--- quillpost/src/services/user-service.js
+++ quillpost/src/services/user-service.js
@@ -6,3 +6,8 @@ export const SignUp = (user) => {
         .post(BASE_URL+'/api/register', user)
         .then((response) => response.data)
 };
+
+export const loginUser = (loginDetails) => {
+    return myAxios.post(BASE_URL+'/api/login', loginDetails)
+        .then((response) => response.data)
+}
