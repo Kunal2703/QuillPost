8cb5bb68cf2080a3b1fe82e20732c462cfe64dde
Pranav Prasanna <77316684+pranavp08@users.noreply.github.com>
2023-11-25 18:23:24 +0530

    Corrected docker compose

--- quillpost/post/blog-post-service/src/main/java/com/quillpost/blogpostservice/config/AppConstants.java
+++ quillpost/post/blog-post-service/src/main/java/com/quillpost/blogpostservice/config/AppConstants.java
@@ -5,6 +5,6 @@ public class AppConstants {
     public static final String PAGE_SIZE="10";
     public static final String SORT_BY="postID";
     public static final String SORT_DIR="asc";
-    public static final String AUTH_URL="http://localhost:8000/api/user";
-    //public static final String AUTH_URL="http://authentication:8000/api/user";
+    //public static final String AUTH_URL="http://localhost:8000/api/user";
+    public static final String AUTH_URL="http://authentication:8000/api/user";
 }
